d8eb5b8e38805a7ca1789f2712d6d65fca26b593
Gustavo C. <gustacavalcantecorrea@gmail.com>
2025-05-19 19:15:57 -0300

    interpretador_brhtml

---
+++ interpretador_brhtml/test/interpretador_brhtml_test.dart
@@ -0,0 +1,9 @@
+import 'package:interpretador_brhtml/interpretador_brhtml.dart';
+// import 'package:interpretador_brhtml/interpretador_brhtml.dart' as interpretador_brhtml;
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
--- interpretador_brhtml/test/interpretador_html_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:interpretador_html/interpretador_html.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
