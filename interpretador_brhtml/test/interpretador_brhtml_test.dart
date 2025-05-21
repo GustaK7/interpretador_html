2db0d241a2460d8e92e99d5816af2b2f17dea4c6
Gustavo C. <gustacavalcantecorrea@gmail.com>
2025-05-21 16:36:22 -0300

    limpando pasta

--- interpretador_brhtml/test/interpretador_brhtml_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:interpretador_brhtml/interpretador_brhtml.dart';
-// import 'package:interpretador_brhtml/interpretador_brhtml.dart' as interpretador_brhtml;
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
